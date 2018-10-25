9c887819b4a7f80cc1750e2c4af13003fa523e2f
almeida-marcos <bonner.marcos@gmail.com>
2018-10-24 21:18:41 -0300

    Adding libs folder

---
+++ libs/blocks.h
@@ -0,0 +1,22 @@
+
+
+class Block{
+
+	private:
+		int x_pos;
+		int y_pos;
+		int width;
+		int height;
+
+	public:
+
+		Block(int x, int y, int w, int h){
+			x_pos = x;
+			y_pos = y;
+			width = w;
+			height = h;
+		}
+
+};
+
+
